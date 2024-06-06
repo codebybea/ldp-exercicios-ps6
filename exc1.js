948137b7a2a61a19ef7e4f922b3a20c6df1a0161
beatrizborges1 <beatriz.borges0407@gmail.com>
2024-06-05 21:26:05 -0300

    add exc

---
+++ exc1.js
@@ -0,0 +1,40 @@
+// Beatriz Alves Borges - 23214290001
+
+// 1 -
+const A = [1, 5, 10, 15, 20, 25 ];
+console.log("1 - ", A);
+
+// 2 -
+const k = 2; //
+const B = A.map(n => n * k);
+console.log("2 - ", B); // multiplica todo o array por k
+
+// 3 -
+const ultimoN = B.pop();
+console.log("3 - ", B); // remove o ultimo numero
+
+// 4 -
+B.unshift(100);
+console.log("4 - ", B); // adiciona um numero no começo do array
+
+// 5 -
+const primeiroN = B.shift();
+console.log("5 -  ", B); // remove o primeiro numero
+
+// 6 -
+B.splice(1, 1);
+console.log("6 - ", B); // remove o segundo numero
+
+// 7 -
+B.splice(1, 0, 10, 20, 30);
+console.log("7 - ", B); // adiciona os 3 numeros no começo do array
+
+// 8 -
+const pares = B.filter((numero) => numero % 2 === 0);
+console.log("8 - ", pares); // faz um novo array com somente os numeros pares
+
+// 9 - 
+const indice = B.indexOf(2);
+console.log("9 - ", indice); // retorna o índice da segunda ocorrência do nuemro
+
+
